cc5111e8d0bcb775bbbf20d7cd69b7abfe375658
weiyu1029 <78815181+weiyu1029@users.noreply.github.com>
2023-11-19 10:19:42 +0800

    Add files via upload

---
+++ devopsdiy-spring2.7.17/src/main/java/com/example/demo/DemoApplication.java
@@ -0,0 +1,20 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@SpringBootApplication
+public class DemoApplication {
+	@GetMapping("/")
+	public String hello() {
+		return "<html><body><h1>Hello DevSecOps</body></h1></html>";
+	}
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoApplication.class, args);
+	}
+
+}
